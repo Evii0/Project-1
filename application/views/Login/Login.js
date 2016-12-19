c8017bec722d05dbcb69f4c1d74458253a137ccd
DESKTOP-3B0AQ28\peter <peter.isnt@gmail.com>
2016-12-20 08:56:47 +1300

    Login gui finished

--- application/views/Login/Login.js
+++ application/views/Login/Login.js
@@ -1,5 +1,5 @@
 function createAccount(){
-    
+    validateEmail(document.getElementById('email').value);
 }
 
 function login(){
@@ -7,6 +7,16 @@ function login(){
 }
 
 function showCreate(){
+    console.log("called");
     document.getElementById('loginContainer').style.display = "none";
     document.getElementById('createContainer').style.display = "inline";
+}
+
+/*
+Validates the given email string using a regex pattern
+Returns bool
+*/
+function validateEmail(email){
+    var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return pattern.test(email);
 }
--- application/views/Login/Login.php
+++ application/views/Login/Login.php
@@ -38,103 +38,59 @@
         <div id="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <?php if(!isset($_SESSION['code'])) { ?>
-                    <div class="panel panel-default">
-                    <div class="panel-heading">
-                    Wanting to start an application?
-                    </div>
-                    <div class="panel-body">
-                    <p>You need to contact your employer for access to this site</p>
-                    </div>
-                    </div>
+                     <div id="outer">
+                        <div id="createContainer">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <p id="title">Create Account</p>
+                                </div>
 
-                    <?php } else { ?>
-
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                        Would you like to start a new application?
+                                <div class="panel-body">
+                                    <form action="" method="POST">
+                                        <div class="form-group input-group">
+                                            <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                                            <input type="text" name="email2" placeholder="Email Address" class="form-control" id="email" />
+                                        </div>
+                                        <div class="form-group input-group">
+                                            <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                                            <input type="password" name="pass1" placeholder="Create Password" class="form-control" id="pass1">
+                                        </div>
+                                        <div class="form-group input-group">
+                                            <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                                            <input type="password" name="pass2" placeholder="Confirm Password" class="form-control" id="pass2">
+                                        </div>
+                                        <input type="hidden" name="formControl" value="signup" />
+                                        <button class="btn btn-default" type="submit">Create Account</button>
+                                    </form>
+                                </div>
+                            </div>  
                         </div>
-                        <div id="loginContainer">
-                            <div class="panel-body">
-                                <form action="" method="POST">
-                                    <div class="form-group input-group<?php if(isset($emailError)) {print " has-error";} ?>">
-                                        <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                                        <input type="text" name="email2" placeholder="Email Address" class="form-control" value="<?php if(isset($_POST['email2'])) {print $_POST['email2'];} ?>" />
-                                    </div>
-                                    <?php if(isset($emailError)) {print '<div class="alert alert-danger">'.$emailError.'</div>'; } ?>
-                                    <div class="form-group input-group<?php if(isset($passwordError)) {print " has-error";} ?>">
-                                        <span class="input-group-addon"><i class="fa fa-key"></i></span>
-                                        <input type="password" name="pass1" placeholder="Create Password" class="form-control">
-                                    </div>
-                                    <div class="form-group input-group<?php if(isset($passwordError)) {print " has-error";} ?>">
-                                        <span class="input-group-addon"><i class="fa fa-key"></i></span>
-                                        <input type="password" name="pass2" placeholder="Confirm Password" class="form-control">
-                                    </div>
-                                    <?php if(isset($passwordError)) {print '<div class="alert alert-danger">'.$passwordError.'</div>'; } ?>
-                                    <input type="hidden" name="formControl" value="signup" />
-                                    <button class="btn btn-default" type="submit">Create Account</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-
-                    <?php } ?>
                     
-                    <div id="createContainer">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <p id="title">Create Account</p>
-                            </div>
+                        <div id="loginContainer">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <p id="title">Skills Login</p>
+                                </div>
 
-                            <div class="panel-body">
-                                <form action="" method="POST">
-                                    <div class="form-group input-group<?php if(isset($emailError)) {print " has-error";} ?>">
-                                        <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                                        <input type="text" name="email2" placeholder="Email Address" class="form-control" value="<?php if(isset($_POST['email2'])) {print $_POST['email2'];} ?>" />
-                                    </div>
-                                    <?php if(isset($emailError)) {print '<div class="alert alert-danger">'.$emailError.'</div>'; } ?>
-                                    <div class="form-group input-group<?php if(isset($passwordError)) {print " has-error";} ?>">
-                                        <span class="input-group-addon"><i class="fa fa-key"></i></span>
-                                        <input type="password" name="pass1" placeholder="Create Password" class="form-control">
-                                    </div>
-                                    <div class="form-group input-group<?php if(isset($passwordError)) {print " has-error";} ?>">
-                                        <span class="input-group-addon"><i class="fa fa-key"></i></span>
-                                        <input type="password" name="pass2" placeholder="Confirm Password" class="form-control">
-                                    </div>
-                                    <?php if(isset($passwordError)) {print '<div class="alert alert-danger">'.$passwordError.'</div>'; } ?>
-                                    <input type="hidden" name="formControl" value="signup" />
-                                    <button class="btn btn-default" type="submit">Create Account</button>
-                                </form>
-                            </div>
-                        </div>  
-                    </div>
+                                <div class="panel-body">
+                                    <form action="" method="POST">
+                                        <div class="form-group input-group">
+                                            <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                                            <input type="text" name="email" placeholder="Email Address" class="form-control" />
+                                        </div>
+                                        <div class="form-group input-group">
+                                            <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                                            <input type="password" name="pass" placeholder="Password" class="form-control">
+                                        </div>
 
-                    <div id="loginContainer">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <p id="title">Skills Login</p>
-                            </div>
-
-                            <div class="panel-body">
-                                <form action="" method="POST">
-                                    <div class="form-group input-group<?php if(isset($loginError)) {print " has-error";} ?>">
-                                        <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                                        <input type="text" name="email" placeholder="Email Address" class="form-control" value="<?php if(isset($_POST['email'])) {print $_POST['email'];} ?>" />
-                                    </div>
-                                    <div class="form-group input-group<?php if(isset($loginError)) {print " has-error";} ?>">
-                                        <span class="input-group-addon"><i class="fa fa-key"></i></span>
-                                        <input type="password" name="pass" placeholder="Password" class="form-control">
-                                    </div>
-
-                                    <?php if(isset($loginError)) {print '<div class="alert alert-danger">Invalid Login</div>'; } ?>
-
-                                    <input type="hidden" name="formControl" value="login" />
-                                    <div id="linksContainer">
-                                        <a href="" onclick="showCreate()" class="link" id="create">Create Account</a>
-                                        <a href="passwordReset.php" class="link" id="forgot">Forgot Password</a>
-                                    </div>
-                                    <button class="btn btn-default" type="submit" id="loginButton">Login</button>
-                                </form>
+                                        <input type="hidden" name="formControl" value="login" />
+                                        <div id="linksContainer">
+                                            <a onclick="showCreate()" class="link" id="create">Create Account</a>
+                                            <a href="passwordReset.php" class="link" id="forgot">Forgot Password</a>
+                                        </div>
+                                        <button class="btn btn-default" type="submit" id="loginButton">Login</button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
