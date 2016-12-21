05bf8157a5c14df3b534f410d6ecdea7ba4b21a2
godfreya <ant.godfrey1@gmail.com>
2016-12-22 10:33:49 +1300

    a small fix

--- application/views/Forms/Training Agreement/GeneralDetails.php
+++ application/views/Forms/Training Agreement/GeneralDetails.php
@@ -2,7 +2,7 @@
   <h1 class="page-header">Trainee Information</h1>
   <div class="contentContainer">
     <form class="form-horizontal" action="form.php?form=TraineeInformation" method="post" enctype="multipart/form-data">
-      <div class="form-group <?php if (isset($salutationError)) print "has-error"; ?>">
+      <div class="form-group">
 				<label for="primaryEthnicity" class="col-lg-2 control-label">Title *</label>
 				<div class="col-lg-10">
 				  <select class="form-control" name="salutation" id="salutation">
@@ -19,19 +19,19 @@
 			  <p class="help-block col-lg-offset-2 col-lg-10">Enter your <b><i><u>full legal name</u></i></b> as it appears on your birth certificate or passport</p>
 				<label for="traineeFirstName" class="col-lg-2 control-label">First name *</label>
 				<div class="col-lg-10">
-				  <input type="text" class="form-control" name="traineeFirstName" placeholder="First name" value="<?php if(isset($traineeFirstName)) {print $traineeFirstName;} ?>" />
+				  <input type="text" class="form-control" name="traineeFirstName" placeholder="First name" value="" />
 				</div>
 			</div>
 			<div class="form-group">
 				<label for="traineeMiddleName" class="col-lg-2 control-label">Middle name</label>
 				<div class="col-lg-10">
-				  <input type="text" class="form-control" name="traineeMiddleName" placeholder="Middle name" value="<?php if(isset($traineeMiddleName)) {print $traineeMiddleName;} ?>" />
+				  <input type="text" class="form-control" name="traineeMiddleName" placeholder="Middle name" value="" />
 				</div>
 			</div>
 			<div class="form-group">
 				<label for="traineeLastName" class="col-lg-2 control-label">Last name *</label>
 				<div class="col-lg-10">
-				  <input type="text" class="form-control" name="traineeLastName" placeholder="Last name" value="<?php if(isset($traineeLastName)) {print $traineeLastName;} ?>" />
+				  <input type="text" class="form-control" name="traineeLastName" placeholder="Last name" value="" />
 				</div>
 			</div>
 
